05da28861704ac234fb1425036816b59c8d50e54
nayab <nayabfatima@gmail.com>
2026-06-01 03:06:48 +0500

    Uploading my project files

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceCartSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
