cdea86cf53b9d8c1f03db5aa39414ac8c993369d
nayab <nayabfatima@gmail.com>
2026-06-01 03:16:51 +0500

    Fixing merge issues

--- obj/Debug/net10.0/ShopSmart.AssemblyInfo.cs
+++ obj/Debug/net10.0/ShopSmart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopSmart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05da28861704ac234fb1425036816b59c8d50e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopSmart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopSmart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
